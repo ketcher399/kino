ce94fa6211c826da0548fefafd94805852252aa5
ketcher399 <124335013+ketcher399@users.noreply.github.com>
2023-02-28 14:49:21 +0500

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -5,3 +5,12 @@ Fancybox.bind('[data-fancybox="gallery"]', {
     );
   },
 });
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YSM0PXYWHK"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YSM0PXYWHK');
+</script>
